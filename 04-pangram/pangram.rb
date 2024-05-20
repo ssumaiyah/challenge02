1b1799c45560f87a0371affbdf00c2fe0d275a47
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-05-20 14:39:01 -0500

    updated pangram_test.rb and pangram.rb file so that all tests get passed

---
+++ 04-pangram/pangram.rb
@@ -0,0 +1,7 @@
+class Pangram
+    def self.is_pangram?(str)
+      alphabet = ('a'..'z').to_a
+      str = str.downcase.gsub(/[^a-z]/, '')
+      alphabet.all? { |letter| str.include?(letter) }
+    end
+  end
