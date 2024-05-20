3026d9341a76e2a6b49494e70156def13e232a85
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-05-20 14:23:04 -0500

    fixed hello_world_instance

--- 02-hello-world-instance/hello_world.rb
+++ 02-hello-world-instance/hello_world.rb
@@ -1,15 +1,13 @@
-# hello_world.rb
-
 class HelloWorld
-    def initialize(name = 'World')
-      @name = name
-    end
-  
-    def hello(other_name = nil)
-      greeting = "Hello, #{@name}."
-      greeting += " My name is #{@name}!" if @name
-      greeting += " My name is #{other_name}!" if other_name
-      greeting
+  def initialize(name = 'World')
+    @name = name
+  end
+
+  def hello(other_name = nil)
+    if other_name
+      "Hello, #{other_name}. My name is #{@name}!"
+    else
+      "Hello, World. My name is #{@name}!"
     end
   end
-  
+end
